b7bd55b76239d6978dc0315206787c5e6c145f89
PyGeek <80580345+willCodeForFood-exe@users.noreply.github.com>
2023-03-08 16:18:06 +0300

    fanout

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -7,19 +7,16 @@ factory.Uri = new("amqps://vzraghwb:N15hjuQFbV18uoVcMsUmYKBWth0dgnwG@cow.rmq2.cl
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
-
-//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
-//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
-
-IBasicProperties properties = channel.CreateBasicProperties();
-properties.Persistent = true;
+channel.ExchangeDeclare(exchange: "fanout-exchange-example", type: ExchangeType.Fanout);
 
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
-    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
-    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties: properties);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba " + i);
+    channel.BasicPublish(
+        exchange: "fanout-exchange-example",
+        routingKey: string.Empty,
+        body: message);
 }
 
 Console.Read();
