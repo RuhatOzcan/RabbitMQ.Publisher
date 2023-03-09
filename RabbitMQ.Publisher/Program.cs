30effc02105f73ba3485922efe02caf762af4aff
PyGeek <80580345+willCodeForFood-exe@users.noreply.github.com>
2023-03-09 13:47:54 +0300

    workerqueue

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -7,19 +7,25 @@ factory.Uri = new("amqps://vzraghwb:N15hjuQFbV18uoVcMsUmYKBWth0dgnwG@cow.rmq2.cl
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
+string queueName = "example-work-queue";
 
-//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
-//channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
-
-IBasicProperties properties = channel.CreateBasicProperties();
-properties.Persistent = true;
+channel.QueueDeclare(
+    queue: queueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
 
 for (int i = 0; i < 100; i++)
 {
     await Task.Delay(200);
+
     byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
-    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties: properties);
+
+    channel.BasicPublish(
+        exchange: string.Empty,
+        routingKey: queueName,
+        body: message);
+
 }
 
 Console.Read();
